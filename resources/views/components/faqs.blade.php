c5f4a8aec444deb5d6d5ecd1a171b367347dfbb0
thepedrocalzado <46518927+thepedrocalzado@users.noreply.github.com>
2022-02-19 16:30:49 +0100

    text

--- resources/views/components/faqs.blade.php
+++ resources/views/components/faqs.blade.php
@@ -4,7 +4,22 @@
         <p class="text-white leading-tight font-bold text-5xl text-center mb-6 mt-9">
             Frequently Asked Questions
         </p>
-        <details class="mb-4 ">
+        <div class="flex">
+
+        </div>
+        <details class="flex mb-4 ">
+            <summary class="font-semibold cursor-pointer bg-lightgreen rounded-md py-2 px-4 shadow shadow-lg text-white leading-tight font-bold shadow-lg">
+                I am new to Crypto…how can I get involved?
+            </summary>
+
+            <div class=" p-4 text-white font-bold leading-tight bg-gray-700 mt-4 border-solid border-1 rounded-2xl shadow-lg">
+                If you are all new to Crypto, the first step is to make yourself with some ETH (Etherium, the second largest Crypto currency). The easiest is to exchange USD, EUR, or any FIAT currency via credit card or bank transfer at one of the major exchanges like Coinbase, Binance, Kraken, etc.
+                Once you receive your ETH in your exchange account, we recommend to transfer the ETH to your own wallet.
+                Since for further transactions a MetaMask wallet comes very handy, this would be our wallet of choice.
+                After transferring the ETH to your MetaMask wallet, you are ready to go. Just click on “get Passport” to purchase your choice of Passport on OpenSea.
+            </div>
+        </details>
+        <details class="flex mb-4 ">
             <summary class="font-semibold cursor-pointer bg-lightgreen rounded-md py-2 px-4 shadow shadow-lg text-white leading-tight font-bold shadow-lg">
                 I am new to Crypto…how can I get involved?
             </summary>
@@ -16,56 +31,6 @@
                 After transferring the ETH to your MetaMask wallet, you are ready to go. Just click on “get Passport” to purchase your choice of Passport on OpenSea.
             </div>
         </details>
-        <details class="mb-4">
-            <summary class="font-semibold cursor-pointer bg-gray-200 rounded-md py-2 px-4">
-                ¿Necesito tener una web propia?
-            </summary>
 
-            <div class=" p-2">
-                No, su carta tendra su propio link para que lo compartas donde quieras.
-            </div>
-        </details>
-        <details class="mb-4">
-            <summary class="font-semibold cursor-pointer bg-gray-200 rounded-md py-2 px-4">
-                ¿Es totalmente gratis la carta digital?
-
-            </summary>
-
-            <div class=" p-2">
-                Sí, existe la opción de carta gratis sin compromiso, al cuál implementamos un anuncio por visitante, pero existe diferentes planes sin anuncios. <a  href="https://lacartagratis.es/planes" class="text-blue-600 underline">Ver planes</a>
-            </div>
-        </details>
-        <details class="mb-4">
-            <summary class="font-semibold cursor-pointer bg-gray-200 rounded-md py-2 px-4">
-                Nos encanta el estilo de los diseños de vuestra web, pero nos gustaría ver otros estilos. ¿Es posible?                                </summary>
-
-            <div class=" p-2">
-                Sí, es posible modificar el estilo por defecto de las cartas, en todas las suscripciones incluimos diferentes estilos temáticos a elección del propietario.
-            </div>
-        </details>
-        <details class="mb-4">
-            <summary class="font-semibold cursor-pointer bg-gray-200 rounded-md py-2 px-4">
-                Quiero empezar a crear mi carta. ¿Cuál es el siguiente paso?
-
-            </summary>
-
-            <div class=" p-2">
-                Detallamos los pasos a seguir para crear su carta digital
-                <ul>
-                    <li>1. Registrarse indicando nombre local, nombre personal, correo etc..</li>
-                    <li>2. Indicar datos del local, logotipo, redes sociales, dirección etc..</li>
-                    <li>3. Añadir Menús, categorias y productos </li>
-                    <li>4. Publicación de su carta y asignación de código QR de forma automática </li>
-                </ul>
-            </div>
-        </details>
-        <details class="mb-4">
-            <summary class="font-semibold cursor-pointer bg-gray-200 rounded-md py-2 px-4">
-                Necesito cambiar los productos de la carta, ¿necesito cambiar el código QR?
-            </summary>
-
-            <div class=" p-2">
-                No, si eres usuario premium y puedes cambiar el link de tu carta, nosotros nos encargamos de que tu QR funcione automaticamente. ¡Así que cambia el link las veces que quieras!                                    </div>
-        </details>
     </div>
 </div>
