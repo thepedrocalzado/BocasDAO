5558d63ee841e009432b1e5a347e3ba69d584c8a
thepedrocalzado <46518927+thepedrocalzado@users.noreply.github.com>
2022-02-19 19:14:11 +0100

    term & conditions push

--- resources/views/components/faqs.blade.php
+++ resources/views/components/faqs.blade.php
@@ -4,33 +4,66 @@
         <p class="text-white leading-tight font-bold text-5xl text-center mb-6 mt-9">
             Frequently Asked Questions
         </p>
-        <div class="flex">
 
-        </div>
-        <details class="flex mb-4 ">
-            <summary class="font-semibold cursor-pointer bg-lightgreen rounded-md py-2 px-4 shadow shadow-lg text-white leading-tight font-bold shadow-lg">
-                I am new to Crypto…how can I get involved?
+            <details class="flex mb-4 max-w-3xl mx-auto">
+                <summary class="font-semibold cursor-pointer bg-lightgreen rounded-md py-2 px-4 shadow shadow-lg text-white leading-tight font-bold shadow-lg max-w-3xl mx-auto">
+                    I am new to Crypto…how can I get involved?
+                </summary>
+
+                <div class=" p-4 text-white font-bold leading-tight bg-gray-700 mt-4 border-solid border-1 rounded-2xl shadow-lg max-w-3xl">
+                    If you are all new to Crypto, the first step is to make yourself with some ETH (Etherium, the second largest Crypto currency). The easiest is to exchange USD, EUR, or any FIAT currency via credit card or bank transfer at one of the major exchanges like Coinbase, Binance, Kraken, etc.
+                    Once you receive your ETH in your exchange account, we recommend to transfer the ETH to your own wallet.
+                    Since for further transactions a MetaMask wallet comes very handy, this would be our wallet of choice.
+                    After transferring the ETH to your MetaMask wallet, you are ready to go. Just click on “get Passport” to purchase your choice of Passport on OpenSea.
+                </div>
+            </details>
+
+        <details class="flex mb-4 max-w-3xl mx-auto">
+            <summary class="font-semibold cursor-pointer bg-lightgreen rounded-md py-2 px-4 shadow shadow-lg text-white leading-tight font-bold shadow-lg max-w-3xl mx-auto">
+                What are NFTs?
             </summary>
 
-            <div class=" p-4 text-white font-bold leading-tight bg-gray-700 mt-4 border-solid border-1 rounded-2xl shadow-lg">
-                If you are all new to Crypto, the first step is to make yourself with some ETH (Etherium, the second largest Crypto currency). The easiest is to exchange USD, EUR, or any FIAT currency via credit card or bank transfer at one of the major exchanges like Coinbase, Binance, Kraken, etc.
-                Once you receive your ETH in your exchange account, we recommend to transfer the ETH to your own wallet.
-                Since for further transactions a MetaMask wallet comes very handy, this would be our wallet of choice.
-                After transferring the ETH to your MetaMask wallet, you are ready to go. Just click on “get Passport” to purchase your choice of Passport on OpenSea.
+            <div class=" p-4 text-white font-bold leading-tight bg-gray-700 mt-4 border-solid border-1 rounded-2xl shadow-lg max-w-3xl">
+                NFT stands for “Non-fungible token” and is a unique, one-of-a-kind digital item that users can buy, own, and trade. Use cases are digital art, digital stamps, digital land, etc. Our NFTs are like a rare piece of art that can also act as a “timeshare membership” card which gets you access to the resort and community exclusives.
             </div>
         </details>
-        <details class="flex mb-4 ">
-            <summary class="font-semibold cursor-pointer bg-lightgreen rounded-md py-2 px-4 shadow shadow-lg text-white leading-tight font-bold shadow-lg">
-                I am new to Crypto…how can I get involved?
+
+        <details class="flex mb-4 max-w-3xl mx-auto">
+            <summary class="font-semibold cursor-pointer bg-lightgreen rounded-md py-2 px-4 shadow shadow-lg text-white leading-tight font-bold shadow-lg max-w-3xl mx-auto">
+                What if the NFTs are sold out?
             </summary>
 
-            <div class=" p-4 text-white font-light leading-tight bg-gray-800 mt-4 border-solid border-1 rounded-2xl shadow-lg">
-                If you are all new to Crypto, the first step is to make yourself with some ETH (Etherium, the second largest Crypto currency). The easiest is to exchange USD, EUR, or any FIAT currency via credit card or bank transfer at one of the major exchanges like Coinbase, Binance, Kraken, etc.
-                Once you receive your ETH in your exchange account, we recommend to transfer the ETH to your own wallet.
-                Since for further transactions a MetaMask wallet comes very handy, this would be our wallet of choice.
-                After transferring the ETH to your MetaMask wallet, you are ready to go. Just click on “get Passport” to purchase your choice of Passport on OpenSea.
+            <div class=" p-4 text-white font-bold leading-tight bg-gray-700 mt-4 border-solid border-1 rounded-2xl shadow-lg max-w-3xl">
+                In case our Passport NFTs have sold out, you can try to purchase one on a secondary market (Rarible, OpenSea, etc). Also, feel free to join the Discord and ask as well!
             </div>
         </details>
+
+        <details class="flex mb-4 max-w-3xl mx-auto">
+            <summary class="font-semibold cursor-pointer bg-lightgreen rounded-md py-2 px-4 shadow shadow-lg text-white leading-tight font-bold shadow-lg max-w-3xl mx-auto">
+                What is a DAO?
+            </summary>
+
+            <div class=" p-4 text-white font-bold leading-tight bg-gray-700 mt-4 border-solid border-1 rounded-2xl shadow-lg max-w-3xl">
+                DAO stands for "Decentralized Autonomous Organization." Our concept allows decentralized decision making and community coordination. Passport holders will be able to make proposals, approvals and overview the execution of non-financial BocasDAO matters.            </div>
+        </details>
+
+        <details class="flex mb-4 max-w-3xl mx-auto">
+            <summary class="font-semibold cursor-pointer bg-lightgreen rounded-md py-2 px-4 shadow shadow-lg text-white leading-tight font-bold shadow-lg max-w-3xl mx-auto">
+                Why Bocas del Toro?
+            </summary>
+
+            <div class=" p-4 text-white font-bold leading-tight bg-gray-700 mt-4 border-solid border-1 rounded-2xl shadow-lg max-w-3xl">
+                Bocas del Toro consists of nine main islands and more than 200 keys. The islands are famous for their beaches, bays, wildlife, and the relaxed way of their people’s life. The province has a total population of approximately 126,000 people. Bocas del Toro borders the Caribbean Sea to the north, sharing the border with Costa Rica.
+
+                Bocas del Toro has been a beach destination for Panamanians for many years. In addition, lately many tourists from Europe and America also visit this beautiful hideaway. Bocas has experienced high growth rates during the last years and is expected to turn into the main tourist destination outside the Panama City area.
+
+                <span class="text-dao">Bocas del Toro is classified as one of the top priorities in the countries tourism development plan.</span>
+
+                Bocas del Toro can be reached by plane from Panama City and San Jose, Costa Rica. Connections by ferry and bus are also available.
+        </details>
+
+
+
 
     </div>
 </div>
