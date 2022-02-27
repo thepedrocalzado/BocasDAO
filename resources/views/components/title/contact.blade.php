5feaf8cbe4cee87576e0edda27389031aebe07d2
thepedrocalzado <46518927+thepedrocalzado@users.noreply.github.com>
2022-02-27 02:45:20 +0100

    update

--- resources/views/components/title/contact.blade.php
+++ resources/views/components/title/contact.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-lightgreen pt-10 pb-10 static " id="passport">
+<div class="bg-lightgreen pt-10 pb-10 static " id="contact">
     <div class="md:max-w-6xl md:mx-auto text-center ">
         <p class="text-white leading-tight font-bold text-5xl">
             Contact
