8adf10485528eae435fd3fde9c02bcc906d2d099
puigpedrocalmar@gmail.com <Kirass99>
2022-03-01 18:55:05 +0100

    route QR

---
+++ resources/views/components/QR/carenero-qr.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/css/app.css" rel="stylesheet">
+
+        <link href="{{ asset('css/app.css')}}" rel="stylesheet">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script src="{{ asset('js/jquery.scrollTo.js')}}"/>
+        <link href="https://unpkg.com/tailwindcss@^2.0/dist/tailwind.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/0.8.2/css/flag-icon.min.css" rel="stylesheet">
+        <script src="https://use.fontawesome.com/5a61dfd9ab.js"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+        <title>BocasDAO</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+    </head>
+
+    <div class="bg-lightgreen pt-10 pb-10 static " id="passport">
+        <div class="md:max-w-6xl md:mx-auto text-center ">
+            <p class="text-white leading-tight font-bold text-5xl">
+                Página carenero
+            </p>
+        </div>
+    </div>
+
+
+    </body>
+</html>
+
+
--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,26 @@ Route::get('/lang/{lang}', function ($lang) {
     return redirect()->back();
 });
 
-Route::get('/dev', function () {
+Route::get('/', function () {
     return view('welcome');
 })->name("welcome");
 
 Route::get('/terms-and-conditions', function () {
     return view('terms');
 })->name("terms");
+
+Route::get('/passport/refugio', function () {
+    return view('/components/QR/refugio-qr');
+})->name("refugio-qr");
+
+Route::get('/passport/carenero', function () {
+    return view('/components/QR/carenero-qr');
+})->name("carener-qr");
+
+Route::get('/passport/bocas', function () {
+    return view('/components/QR/bocas-qr');
+})->name("bocas-qr");
+
+Route::get('/passport/panama', function () {
+    return view('/components/QR/panama-qr');
+})->name("panama-qr");
