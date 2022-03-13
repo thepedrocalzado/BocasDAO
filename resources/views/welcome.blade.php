930d5f34b668d53e79b93b3170433d54eda46056
thepedrocalzado <46518927+thepedrocalzado@users.noreply.github.com>
2022-03-13 14:41:15 +0100

    update contact and faqs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,6 @@
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
         <script src="https://cdn.tailwindcss.com"></script>
         <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
-
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
         <title>BocasDAO</title>
         <!-- Fonts -->
@@ -51,7 +50,6 @@
            font-family: font-meddao;
            src: url('{{ asset('fonts/medium.ttf') }}') format("truetype");
        }
-
 
        .font-medao {
            font-family: font-meddao;
