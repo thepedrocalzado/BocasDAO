3301ddc8e9d86fb8ff0f908c0625d601b3510776
thepedrocalzado <46518927+thepedrocalzado@users.noreply.github.com>
2022-03-13 13:26:00 +0100

    update contact and faqs

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Mail;
+use Illuminate\Support\Facades\Session;
+
+class ContactController extends Controller
+{
+    public function contact()
+    {
+        return view('/welcome');
+    }
+
+    public function contactSubmit(Request $request)
+    {
+        Mail::send('email.mail',[
+            'name' => $request->name,
+            'email' => $request->email,
+            'message' => $request->message
+        ],function($mail) use($request){
+            $mail->from(env('MAIL_FROM_ADDRESS'),$request->email);
+            $mail->to('contact@bocasdao.finance')->subject('BocasDao - Contact web');
+            Session::flash('message', '	👋 Gracias por ponerte en contacto conmigo!! En breve le responderé.. ');
+        });
+
+        return redirect()->route('contact');
+
+    }
+
+
+
+}
