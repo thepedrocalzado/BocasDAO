c39720a18bcec4551820d7e06966f8e1ea71ca8b
thepedrocalzado <46518927+thepedrocalzado@users.noreply.github.com>
2022-03-13 14:01:31 +0100

    update contact and faqs

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -17,7 +17,6 @@ class ContactController extends Controller
         ],function($mail) use($request){
             $mail->from(env('MAIL_FROM_ADDRESS'),$request->email);
             $mail->to('contact@bocasdao.finance')->subject('BocasDao - Contact web');
-            Session::flash('message', '	👋 Gracias por ponerte en contacto conmigo!! En breve le responderé.. ');
         });
 
     }
