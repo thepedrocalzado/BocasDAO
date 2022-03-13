079866e0955b329d7ec6d125808b5b56b12fc902
thepedrocalzado <46518927+thepedrocalzado@users.noreply.github.com>
2022-03-13 14:06:58 +0100

    update contact and faqs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,6 +78,7 @@
     @extends('components/content')
         @extends('components/topsection')
         @extends('components/navbar')
-    <script id="CookieDeclaration" src="https://consent.cookiebot.com/1d69968d-d274-4615-9aec-b611433f2616/cd.js" type="text/javascript" async></script>
     </body>
+    <script id="CookieDeclaration" src="https://consent.cookiebot.com/1d69968d-d274-4615-9aec-b611433f2616/cd.js" type="text/javascript" async></script>
+
 </html>
