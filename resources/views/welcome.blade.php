fc1a37d11f4072b7f55fc4663c40fef9fc2322b7
thepedrocalzado <46518927+thepedrocalzado@users.noreply.github.com>
2022-03-13 16:26:23 +0100

    update contact and faqs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/css/app.css" rel="stylesheet">
+
         <link href="{{ asset('css/app.css')}}" rel="stylesheet">
-        <link href="{{ URL::asset('css/app.css')}}" rel="stylesheet">
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script src="{{ asset('js/jquery.scrollTo.js')}}"/>
         <link href="https://unpkg.com/tailwindcss@^2.0/dist/tailwind.min.css" rel="stylesheet">
@@ -12,12 +13,13 @@
         <script src="https://use.fontawesome.com/5a61dfd9ab.js"></script>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
         <script src="https://cdn.tailwindcss.com"></script>
-        <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
         <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
         <title>BocasDAO</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="1d69968d-d274-4615-9aec-b611433f2616" data-blockingmode="auto" type="text/javascript"></script>
 
     </head>
     <script>
@@ -51,6 +53,7 @@
            src: url('{{ asset('fonts/medium.ttf') }}') format("truetype");
        }
 
+
        .font-medao {
            font-family: font-meddao;
        }
@@ -76,5 +79,6 @@
     @extends('components/topsection')
     @extends('components/navbar')
     </body>
+    <script id="CookieDeclaration" src="https://consent.cookiebot.com/1d69968d-d274-4615-9aec-b611433f2616/cd.js" type="text/javascript" async></script>
 
 </html>
