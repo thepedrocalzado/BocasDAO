c64d95ce0f588b2122faac47a432ccf8792d92f7
thepedrocalzado <46518927+thepedrocalzado@users.noreply.github.com>
2022-03-23 20:48:30 +0100

    update contact and faqs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <link rel="canonical" href="{{url()->current()}}"/>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="{{ asset('css/app.css')}}" rel="stylesheet">
+    <link href="css/app.css" rel="stylesheet">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/favicon/apple-touch-icon.png')}}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon/favicon-32x32.png')}}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon/favicon-16x16.png')}}">
